3fddb7c53dee28e555f73e4f7bf4d94ee76b3bf2
JaydenSmith43 <123499904+JaydenSmith43@users.noreply.github.com>
2024-11-30 18:42:21 -0700

    particle tweks

--- Scenes/effects/hit_particle.gd
+++ Scenes/effects/hit_particle.gd
@@ -5,8 +5,6 @@ extends Node3D
 func _network_spawn(data: Dictionary) -> void:
 	global_position.x = (data['pos_x'])
 	global_position.y = -(data['pos_y'])
-	print(position.x)
-	print(position.y)
 	network_timer.start()
 
 func _on_network_timer_timeout() -> void:
